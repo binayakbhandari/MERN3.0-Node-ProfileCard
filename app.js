b8d3e0e1780ae0f224f7605fb4d9992dbd9d8133
binayakbhandari <binayakbhandari60@gmail.com>
2024-10-02 03:55:41 +0545

    Project Hosting online

--- app.js
+++ app.js
@@ -89,7 +89,8 @@ app.get('/person/:id',async (req,res)=>{
 app.delete("/person/:id", async (req, res) => {
     const id = req.params.id
     const person = await Person.findById(id)
-    const imageName = person.personImage.split('/').pop()
+    // const imageName = person.personImage.split('/').pop()
+    const imageName = person.personImage
 
     if(imageName){
         fs.unlink(`storage/${imageName}`, (err) => {
@@ -117,7 +118,8 @@ app.patch("/person/:id",upload.single('personImage'),async (req,res)=>{
     if(req.file){
         filename = "https://mern3-0-node-profilecard.onrender.com/" + req.file.filename
         const person = await Person.findById(id)
-        const imageName = person.personImage.split('/').pop()
+        // const imageName = person.personImage.split('/').pop()
+        const imageName = person.personImage
         console.log(imageName)
         fs.unlink( `storage/${imageName}`, (err)=>{
             if(err){
